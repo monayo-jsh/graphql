5d52e3dd6ac93108edfbae665ebb84b4ac89fe56
주상현 <monayo.jsh@gmail.com>
2023-09-19 15:06:40 +0900

    update index.ts and create new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 // 1
 import { schema } from "./schema";
 import { context } from "./context"
 export const server = new ApolloServer({
     schema,
-    context
+    context,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()]
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // 2
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
